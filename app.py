4022fdb191276193fd51e4890f984edcb6b72ead
Alexandre Ricardo Dezembro <40897200+adezembro@users.noreply.github.com>
2025-08-11 16:11:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide",
 )
 # --- Carregamento dos dados ---
-df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/adezembro/idcp-alura/refs/heads/A7X3D9/dados-imersao-final.csv")
 
 
 # --- Barra Lateral (Filtros) ---
